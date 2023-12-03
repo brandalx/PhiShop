becffddf178ae9b4daa8059a1764ea3f00ae8ce7
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 13:21:06 -0500

    feat: navbar initial ui, icons init

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center ">
+              {/* todo: mobile navbar */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">Icon</Link>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;

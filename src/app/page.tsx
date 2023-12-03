968e3a088b8fa44cdc60bce82b88122a2a67eaf9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 00:14:37 -0500

    feat: ui for home page, button init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        sss
+        <h1 className="text-4xl font-bold trackging-tiht  text-gray-900 sm:text-6xl">
+          PhiShop is an online marketplace for high-quality
+          <span className="text-blue-600"> digital assets</span>
+        </h1>
+        <p className="mt-6 text0lg max-w-prose text-muted-foreground">
+          Welcome to PhiShop. Every product on our platform is verified by our
+          team and totally safe to buy.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6 ">
+          <Link href="/prodiucts" className="">
+            Browse trending products
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );

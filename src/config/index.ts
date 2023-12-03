29f4db57ce2c527c86c78fe6ddfa6be5ace5f774
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 13:42:38 -0500

    feat: products Icons

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,21 @@ export const PRODUCT_CATEGORIES = [
       { name: "Best sellers", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
     ],
   },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      { name: "New Arrivals", href: "#", imageSrc: "/nav/icons/new.jpg" },
+      {
+        name: "Best Selling Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
 ];

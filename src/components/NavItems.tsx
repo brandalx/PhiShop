2735c740b0c47c1df532c1461c12e7ef17f83134
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 14:15:41 -0500

    feat: useonclickoutse hook,

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import NavItem from "./NavItem";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
   const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>();
   return (
     <div className="flex gap-4 h-full">
       {PRODUCT_CATEGORIES.map((category, i) => {

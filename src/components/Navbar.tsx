93b66b328f27cbdba78dfe2f52e867164e9e36f3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 23:03:30 -0500

    feat: cond render for navbar sign in / sign up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
+  // mock data
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16 ">
       <header className="relative bg-white">
@@ -19,6 +23,33 @@ const Navbar = () => {
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in "
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      className={buttonVariants({ variant: "ghost" })}
+                      href="/sign-up"
+                    >
+                      Create account
+                    </Link>
+                  )}
+                </div>
               </div>
             </div>
           </div>

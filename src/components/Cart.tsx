553650b3727235d44597e1725066ee924deba523
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-03 23:17:07 -0500

    feat: mobile slidebar trigger layout

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { ShoppingCartIcon } from "lucide-react";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCartIcon
+          aria-hidden="true"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-blue-500 transition-all"
+        />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-blue-800">
+          0
+        </span>
+      </SheetTrigger>
+    </Sheet>
+  );
 };
 
 export default Cart;

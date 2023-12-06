6c930f317e3639c04c8d61fa68c3b7ab5303d1e4
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-06 13:31:13 -0500

    feat: get payload for db server

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
-
+import type { InitOptions } from "payload/config";
+import payload from "payload";
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
@@ -11,9 +12,29 @@ if (!cached) {
     client: null,
     promise: null,
   };
-
-  interface Args {
-    initOptions?: Partial<InitOptions>;
-  }
 }
-export const getPayLoadClient = async ({ initOptions }: Args) => {};
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+export const getPayLoadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("Payload_secret is missing");
+  }
+  if (cached.client) {
+    return cached.client;
+  }
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    throw e;
+  }
+  return cached.client;
+};

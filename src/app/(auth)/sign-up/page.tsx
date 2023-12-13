fedaa325d52bdd49ee7a622324da1691dd51a975
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-13 15:52:01 -0500

    feat:additional error handling, on submit redirect

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -14,8 +14,11 @@ import {
 } from "@/lib/validators/account-credentials-validator";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import { ZodError } from "zod";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -27,7 +30,19 @@ const Page = () => {
     onError: (err) => {
       if (err.data?.code === "CONFLICT") {
         toast.error("This email is already in use. Sign in intead");
+        return;
       }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message);
+        return;
+      }
+
+      toast.error("Something went wrong, please try again");
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}`);
+      router.push("/verify-email?to=" + sentToEmail);
     },
   });
   const onSubmit = ({ email, password }: TAuthCredentialsValidation) => {

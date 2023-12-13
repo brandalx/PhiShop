7ee7d315703f2201731318c7155eb854c163c450
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-13 01:46:43 -0500

    fix: verify email

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -41,7 +41,7 @@ export const getPayLoadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: "brandon@brandnolandev.com",
+        fromAddress: "verify@brandnolandev.com",
         fromName: "PhiShop",
       },
       secret: process.env.PAYLOAD_SECRET,

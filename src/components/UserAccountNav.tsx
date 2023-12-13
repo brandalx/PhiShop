0229415c92bf977922c6c5fb4082959f3e749fb2
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-13 16:56:44 -0500

    feat: initial ui for UserAccountNav

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,25 @@
 "use client";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const UserAccountNav = () => {
-  return <div>UserAccountNav</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button variant="ghost" size="sm" className="relative">
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white w-60" align="end">
+        <div className="flex items-center justify-start gap-2 p-2"></div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserAccountNav;

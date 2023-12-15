4e2ff7bfefe35537dafd4ca26548f639459abbf5
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-15 17:02:18 -0500

    feat: getInfiniteProducts fetching db with query

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,16 +1,37 @@
 import { z } from "zod";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "@/lib/validators/query-validator";
+import { getPayLoadClient } from "../get-payload";
 
 export const appRouter = router({
   auth: authRouter,
-  getInfiniteProducts: publicProcedure.input(
-    z.object({
-      limit: z.number().min(1).max(100),
-      cursor: z.number().nullish(),
-      query: QueryValidator,
-    })
-  ),
+  getInfiniteProducts: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(),
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input;
+      const { sort, limit, ...queryOpts } = query;
+
+      const payload = await getPayLoadClient();
+
+      const { docs } = await payload.find({
+        collection: "products",
+        where: {
+          approvedForSale: {
+            equals: "approved",
+          },
+        },
+        sort,
+        depth: 1,
+        limit,
+      });
+    }),
 });
 
 export type AppRouter = typeof appRouter;

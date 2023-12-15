b5e9bd45a51299b62284639e43dfc863d0438d81
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-15 17:07:11 -0500

    feat: getInfiniteProducts  ready

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
-import { QueryValidator } from "@/lib/validators/query-validator";
+import { QueryValidator } from "../lib/validators/query-validator";
 import { getPayLoadClient } from "../get-payload";
 
 export const appRouter = router({
@@ -19,18 +19,37 @@ export const appRouter = router({
       const { sort, limit, ...queryOpts } = query;
 
       const payload = await getPayLoadClient();
+      const parseQueryOpts: Record<string, { equlas: string }> = {};
 
-      const { docs } = await payload.find({
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parseQueryOpts[key] = {
+          equlas: value,
+        };
+      });
+
+      const page = cursor || 1;
+
+      const {
+        docs: items,
+        hasNextPage,
+        nextPage,
+      } = await payload.find({
         collection: "products",
         where: {
           approvedForSale: {
             equals: "approved",
           },
+          ...parseQueryOpts,
         },
         sort,
         depth: 1,
         limit,
+        page,
       });
+      return {
+        items,
+        nextPage: hasNextPage ? nextPage : null,
+      };
     }),
 });
 

ddb4ba0d967274277ca64d67bef8d4f8e992dbc9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 18:57:30 -0500

    feat: product image slider with swiper js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ["static.vecteezy.com", "www.lapa.ninja", "lapa.ninja"],
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "**",
+        port: "3000",
+        protocol: "http",
+      },
+    ],
   },
 };
 

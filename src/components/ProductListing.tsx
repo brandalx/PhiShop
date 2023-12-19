bc567c5068bdc759882f3568cef0b5e72c8ba38f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 18:17:15 -0500

    feat: ProductPlaceHolder, skeleton init

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { Product } from "@/payload-types";
-import React from "react";
+import React, { useState } from "react";
 interface ProductListingProps {
   product: Product | null;
   index: number;
 }
-const ProductListing = ({}: ProductListingProps) => {
+const ProductListing = ({ product, index }: ProductListingProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const ProductPlaceHolder = () => {
+    return (
+      <div className="flex flex-col w-full">
+        <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl"></div>
+      </div>
+    );
+  };
+  if (!product || isVisible) return <ProductPlaceHolder />;
+
   return <div>ProductListing</div>;
 };
 

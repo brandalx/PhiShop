187253168abd3f00baf352f19eeca13e37c61be9
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 18:42:05 -0500

    feat: product listing ui price, info, loaders

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -67,7 +67,7 @@ const ProudctReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {map.map((product, i) => (
-              <ProductListing key={product?.id} />
+              <ProductListing key={product?.id} index={i} product={product} />
             ))}
           </div>
         </div>

63c40407f7222c23959cf6b23685cfa6f6be74a1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 23:26:45 -0500

    feat: use cart hook done

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -4,15 +4,37 @@
 
 import { Product } from "@/payload-types";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 export type CartItem = {
   product: Product;
 };
 type CartState = {
-  items: CartItem;
+  items: CartItem[];
   addItem: (product: Product) => void;
-  removeItem: (productId: Product) => void;
+  removeItem: (productId: string) => void;
   clearCart: () => void;
 };
 
 //track of cart items
-export const useCart = create<CartState>();
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          return {
+            items: [...state.items, { product }],
+          };
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((items) => items.product.id !== id),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);

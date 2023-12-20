7748899002ec957479a75569161a633d06f1ae08
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 23:44:15 -0500

    feat: CartItem add, remove, display

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -27,6 +27,12 @@ const Cart = () => {
     (total, { product }) => total + product.price,
     0
   );
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -35,14 +41,14 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-blue-500 transition-all"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-blue-800">
-          {itemCount}
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart ({itemCount})</SheetTitle>
+          <SheetTitle>Cart ({isMounted ? itemCount : 0})</SheetTitle>
         </SheetHeader>
-        {itemCount > 0 ? (
+        {itemCount > 0 && isMounted ? (
           <>
             <div className="flex w-full flex-col pr-6">
               <ScrollArea>

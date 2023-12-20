14932a6f732dd93902b3278c0e79d4a505989a65
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-19 23:09:33 -0500

    feat: AddToCartButton ui

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 const AddToCartButton = () => {
-  return <div>AddToCartButton</div>;
+  const [isSuccess, setisSuccess] = useState<boolean>(false);
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setisSuccess(false);
+    }, 2000);
+    return () => clearTimeout(timeOut);
+  }, [isSuccess]);
+
+  return (
+    <Button
+      onClick={() => {
+        setisSuccess(true);
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? "Added!" : "Add to cart"}
+    </Button>
+  );
 };
 
 export default AddToCartButton;

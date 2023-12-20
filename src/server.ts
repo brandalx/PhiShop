d9f00b6385e52495978d52db6efceee10935c4e1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-20 02:25:31 -0500

    feat: webhooks, server webhookMiddleware

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,40 @@ import { nextApp, nextHandler } from "./next-utlis";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
-const app = express();
+import bodyParser from "body-parser";
+import { IncomingMessage } from "http";
+import { stripeWebhookHandler } from "./webhooks";
+import nextBuild from "next/dist/build";
+import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
+const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 const createContext = ({
   req,
   res,
-}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
+
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer;
+};
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayLoadClient({
     initOptions: {
       express: app,
@@ -23,6 +47,35 @@ const start = async () => {
     },
   });
 
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info("Next.js is building for production");
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+
+      process.exit();
+    });
+
+    return;
+  }
+
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+    const parsedUrl = parse(req.url, true);
+    const { query } = parsedUrl;
+
+    return nextApp.render(req, res, "/cart", query);
+  });
+
+  app.use("/cart", cartRouter);
   app.use(
     "/api/trpc",
     trpcExpress.createExpressMiddleware({
@@ -30,9 +83,12 @@ const start = async () => {
       createContext,
     })
   );
+
   app.use((req, res) => nextHandler(req, res));
+
   nextApp.prepare().then(() => {
     payload.logger.info("Next.js started");
+
     app.listen(PORT, async () => {
       payload.logger.info(
         `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
